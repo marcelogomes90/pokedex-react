721fc301fbfc24e2fd33a4d178770cbf9766cb01
Marcelo Gomes <94990663+marcelogomes90@users.noreply.github.com>
2022-05-26 12:29:18 -0300

    change the image url

--- Pokedex/src/components/CardContent/index.js
+++ Pokedex/src/components/CardContent/index.js
@@ -27,7 +27,7 @@ function CardContent(props) {
             
             {firstType?.filter(pokemon => pokemon.slot == 1).map(poketype => (
                 <div className={`${poketype.type.name}-bg`}>
-                    <img className="pokemon-img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ids}.svg`}></img>
+                    <img className="pokemon-img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${ids}.png`}></img>
                 </div>   
             ))}
 
